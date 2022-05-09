3e0cbb8ac8e0a5e148a374d291a592151091e411
nuviedo <alexis.nuviedo@outlook.com>
2022-05-09 10:29:01 -0500

    01

--- 01_Database/dbcon.py
+++ 01_Database/dbcon.py
@@ -1,1 +1,15 @@
+#import psycopg2
 
+
+dconf={}
+with open("db_logn.conf","r") as f:
+	L_CONF=[]
+	for l in f.read().split("\n"):
+		if(len(l)<1):
+			continue
+		L_CONF.append(l)
+	f.close()
+	dconf={L.split(":")[0]:L.split(":")[1] for L in L_CONF}
+#print(dconf)
+
+con=psycopg2.connect(host=dconf["host"],port=dconf["port"],database=dconf["database"],user=dconf["user"],password=dconf["password"])
