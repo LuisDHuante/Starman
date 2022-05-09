97bd878364ab29c8073f6e7c805f79602a8ce13f
nuviedo <alexis.nuviedo@outlook.com>
2022-05-09 10:34:26 -0500

    01

--- 01_Database/dbcon.py
+++ 01_Database/dbcon.py
@@ -15,3 +15,4 @@ with open("db_logn.conf","r") as f:
 con=psycopg2.connect(host=dconf["host"],port=dconf["port"],database=dconf["database"],user=dconf["user"],password=dconf["password"])
 cur=con.cursor()
 cur.execute("SELECT * FROM Flights;")
+print(cur.fetchall())
