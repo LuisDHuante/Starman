41af2d1631728721412b12f107b75b85172669ed
nuviedo <alexis.nuviedo@gmail.com>
2022-05-10 16:19:48 -0500

    02

--- 01_Database/dbcon.py
+++ 01_Database/dbcon.py
@@ -23,10 +23,10 @@ def dump_csv():
     
     FL=read_dict(cflr)
     
-    DF.drop(["0","sensors","geo_altitude","squawk","spi","position_source","vertical_rate","baro_altitude","last_contact"],axis=1,inplace=True)
+    DF.drop(["sensors","geo_altitude","squawk","spi","position_source","vertical_rate","baro_altitude","last_contact"],axis=1,inplace=True)
     DF.dropna(inplace=True)
     DF=DF[DF["on_ground"]==False].copy()
-    
+    print(DF.columns())
     print(DF.info())
     print(FL)
 
