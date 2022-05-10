69583394a21dc8b57a8173a2c90a101d6eeec645
nuviedo <alexis.nuviedo@gmail.com>
2022-05-10 15:56:08 -0500

    02

--- 01_Database/dbcon.py
+++ 01_Database/dbcon.py
@@ -1,25 +1,33 @@
 import psycopg2
 
 
-dconf={}
-with open("db_logn.conf","r") as f:
-	L_CONF=[]
-	for l in f.read().split("\n"):
-		if(len(l)<1):
-			continue
-		L_CONF.append(l)
-	f.close()
-	dconf={L.split(":")[0]:L.split(":")[1] for L in L_CONF}
-#print(dconf)
+def read_dict():
+    with open(path,"r") as f:
+        L_CONF=[]
+        for l in f.read().split("\n"):
+            if(len(l)<1):
+                continue
+            L_CONF.append(l)
+        f.close()
+        dconf={L.split(":")[0]:L.split(":")[1] for L in L_CONF}
+
+ccsv="./../current.csv"
+cflr="./../current.flr"
+def dump_csv():
+    DF=pd.read_csvA(ccsv)
+    FL=read_dict(cflr)
+    print(DF.info())
+    print(FL)
+
+dconf=read_dict("db_logn.conf")
 
 con=psycopg2.connect(host=dconf["host"],port=dconf["port"],database=dconf["database"],user=dconf["user"],password=dconf["password"])
 cur=con.cursor()
+
 try:
-	cur.execute("SELECT * FROM Flights;")
-	print(cur.fetchall())
+	dump_csv()
 except:
 	pass
-
 
 con.commit()
 cur.close()
