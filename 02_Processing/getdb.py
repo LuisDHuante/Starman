c12b33a9c13b4f0b4d350b2887c53e8615957f9e
nuviedo <alexis.nuviedo@gmail.com>
2022-06-07 23:49:14 -0500

    Mapmaking update

--- 02_Processing/getdb.py
+++ 02_Processing/getdb.py
@@ -17,30 +17,7 @@ def read_dict(path):
         ret={L.split(":")[0]:L.split(":")[1] for L in L_CONF}
     return ret
 
-cols=["icao24", "callsign", "origin_country", "time_position", "longitude", "latitude", "velocity", "true_track"]
-def dump_csv(con,cur):
-    DF=pd.read_csv(ccsv)
-    
-    FL=read_dict(cflr)
-    
-    DF.drop(["Unnamed: 0","sensors","geo_altitude","squawk","spi","position_source","vertical_rate","baro_altitude","last_contact"],axis=1,inplace=True)
-    DF.dropna(inplace=True)
-    DF=DF[DF["on_ground"]==False].copy()
-    DF["time_position"]=DF["time_position"].astype('int')
-    DF.drop(["on_ground"],axis=1,inplace=True)
-    
-    #print(DF.info())
-    #print(FL)
-    
-    cur.execute("DELETE FROM Flights;")
-    T=[]
-    for t in DF.iterrows():
-        S=t[1]
-        L=[]
-        for c in cols:
-            L.append(S[c])
-        T.append(tuple(L))
-    psycopg2.extras.execute_values(cur,f"INSERT INTO Flights VALUES %s;",T)
+
 dconf=read_dict(f"{base}../01_Database/db_logn.conf")
 
 con=psycopg2.connect(host=dconf["host"],port=dconf["port"],database=dconf["database"],user=dconf["user"],password=dconf["password"])
@@ -51,7 +28,7 @@ curf=conf.cursor()
 #try:
 cur.execute("SELECT * FROM flights;")
 flights=cur.fetchall()
-curf.execute("SELECT * FROM flares")
+curf.execute("SELECT * FROM flares;")
 flares=curf.fetchall()
 
 conf.commit()
